a549100aaf082495c1f378209e43cae03fdd5311
nagarjunaprasad98 <nagarjunaprasad99@gmail.com>
2024-11-23 23:32:18 +0530

    database changes

--- index.js
+++ index.js
@@ -12,9 +12,14 @@ const PORT = process.env.PORT || 8000;
 const MONGOURL = process.env.MONGO_URL;
 
 mongoose
-  .connect(MONGOURL)   
+  .connect(MONGOURL)
   .then(() => {
     console.log("Database connected successfully");
+    console.log(
+      "Data Base Connected::",
+      mongoose.connection.host,
+      mongoose.connection.name
+    );
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
